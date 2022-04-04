33154be9bf9e5599be2a12bd4c0c22d285aa3b22
sstormblessed <nikovazquez52@gmail.com>
2022-04-04 10:39:49 +0200

    primer commit

---
+++ src/main/java/com/example/demo/controllers/Ejercicio.java
@@ -0,0 +1,30 @@
+package com.example.demo.controllers;
+
+import com.example.demo.utils.Utils;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Ejercicio {
+    // http://localhost:8080/
+    @GetMapping("/")
+    public String greet() {
+        return "Bienvenido al servidor backend";
+    }
+
+    // http://localhost:8080/aleatorio
+    @GetMapping("/aleatorio")
+    public String numeroRandom() {
+        long r = Math.round(Math.random() * 100);
+        return r + "";
+    }
+
+    // http://localhost:8080/palindromo/ana
+    @GetMapping("/palindromo/{name}")
+    public String isPalindrome(@PathVariable String name) {
+        boolean palindrome = Utils.isPalindrome(name);
+        return palindrome ? "Si es palíndromo" : "No es palíndromo";
+    }
+}
